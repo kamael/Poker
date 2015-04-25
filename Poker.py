e885adab76d2fc0d991f74d779718d1d95e47f3e
myvyang <myvyangQ@gmail.com>
2015-04-25 14:26:32 +0800

    add agent

--- Poker.py
+++ Poker.py
@@ -37,10 +37,17 @@ class Poker:
         if x > 52:
             return x
         x = x % 13
+        """
         if x == 0:
             x = 13
         if x < 3:
             return x + 20
+        return x
+        """
+        if x == 2:
+            x = 20
+        elif x < 3:
+            x = x + 13
         return x
 
     def sendCards(self):
@@ -83,9 +90,181 @@ class Poker:
         print sorted(self.pValue)
 
 
-if __name__ == "__main__":
+class Agent():
+
+    def __init__(self, values):
+        self.values = values
+        self.update()
+
+    def send(self, value):
+        values_bak = self.values
+        for i in value:
+            try:
+                self.values.remove(i)
+            except ValueError:
+                print "value %s not in Agent's list"
+                self.values = values_bak
+                return False
+        self.update()
+        return True
+
+    def add(self, value):
+        self.values += value
+        self.update()
+
+    def update(self):
+        self.__process()
+
+
+    def __process(self):
+        self.values.sort()
+
+        self.singles = set()
+        self.twices = set()
+        self.triples = set()
+        self.fours = set()
+
+        self.shunzi_s = []
+        self.shunzi_ss = []
+        self.shunzi_sss = []
+
+
+        numbers = set(self.values)
+
+        for i in numbers:
+            i_count = self.values.count(i)
+
+            if i_count == 4:
+                self.fours.add(i)
+            if i_count >= 3:
+                self.triples.add(i)
+            if i_count >= 2:
+                self.twices.add(i)
+            self.singles.add(i)
+
+
+        base_shunzi = [sorted(list(self.singles)),
+                       sorted(list(self.twices)),
+                       sorted(list(self.triples))]
+        store_shunzi = [self.shunzi_s, self.shunzi_ss, self.shunzi_sss]
+        len_shunzi = [5, 3, 2]
+
+        for i in range(3):
+            if not base_shunzi[i]:
+                continue
+            start = base_shunzi[i][0]
+            n = 1
+
+            for j in base_shunzi[i]:
+                if j == start:
+                    pass
+                elif j == start + 1:
+                    n += 1
+                else:
+                    if n >= len_shunzi[i]:
+                        store_shunzi[i].append([start - n + 1, n])
+                    n = 1
+
+                start = j
+
+            if n >= len_shunzi[i]:
+                store_shunzi[i].append([start - n + 1, n])
+
+    def choose_dz(self, pValue):
+        if random.randint(0, 2) == 0:
+            self.add(pValue)
+            return True
+        return False
+
+    def action(self, game):
+        i = 1
+        b = 2
+
+
+        if random.randint(0, 30) == 0:
+            game.end = True
+
+
+
+
+class Game():
+    def __init__(self):
+        self.end = False
+        self.winner = None
+        self.poker = Poker()
+        self.ag1 = Agent(self.poker.aValue)
+        self.ag2 = Agent(self.poker.bValue)
+        self.ag3 = Agent(self.poker.cValue)
+        self.agents = [self.ag1, self.ag2, self.ag3]
+
+        self.dz_index = None
+        self.__choose_dz()
+
+    def __choose_dz(self):
+        did = False
+        for i in range(3):
+            did = self.agents[i].choose_dz(self.poker.pValue)
+            if did:
+                self.dz_index = i
+                break
+        if not did:
+            self.winner = -1
+            self.dz_index = -1
+            self.end = True
+
+
+
+def main():
+    game = Game()
+    print "dz: %d" % game.dz_index
+    for i in range(3):
+        cur = (i + game.dz_index) % 3
+        print "%d has: %s" % (cur, str(game.agents[cur].values))
+
+    while not game.end:
+        for i in range(3):
+            cur = (i + game.dz_index) % 3
+            print "agent %d is action" % cur
+            game.agents[cur].action(game)
+            if game.end:
+                game.winner = cur
+                break
+
+    print "end"
+    print "winner: %d" % game.winner
+
+
+def main1():
+    #ag = Agent([3, 5, 5, 6, 7, 8, 8, 9, 9, 10, 10, 10, 11, 11, 14, 20, 20])
+    #print ag.shunzi_ss
+    #ag = Agent([3, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 10, 11, 11, 12, 13, 13])
+    #print ag.shunzi_ss
+    #ag = Agent([3, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 10, 11, 11, 12, 12, 13, 13])
+    #print ag.shunzi_ss
+    #ag = Agent([3, 3, 4, 4, 4, 5, 5, 6, 7, 7, 7, 10, 11, 11, 12, 12, 12, 13, 13, 13])
+    #print ag.shunzi_ss
+    #print ag.shunzi_sss
+    #ag = Agent([3, 3, 4, 5, 5, 6, 6, 7, 8, 9, 9, 10, 11, 11, 12, 12, 13, 13, 20, 20])
+    #print ag.shunzi_ss
+
     poker = Poker()
-    poker.cardnumPrint()
-    poker.cardvaluePrint()
+    Ag1 = Agent(poker.aValue)
+    Ag2 = Agent(poker.bValue)
+    Ag3 = Agent(poker.cValue)
+    Ag1.add(poker.pValue)
+
+    for item in [Ag1, Ag2, Ag3]:
+        print sorted(item.values)
+        print item.singles
+        print item.twices
+        print item.triples
+        print item.fours
+        print item.shunzi_s
+        print item.shunzi_ss
+        print item.shunzi_sss
 
 
+
+
+if __name__ == "__main__":
+    main()
